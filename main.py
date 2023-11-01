f76e54cf953b286202eef65c662eb7333acc6150
Lokesh <lokeshb031@gmail.com>
2023-11-02 03:50:46 +0530

    Fixed temperature reading and name error.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from backend import get_data
 
 st.title("Weather forecast for the Next Days")
 place = st.text_input("Place: ")
+
 days = st.slider("Forecast Days", min_value=1,
                  max_value=5, help="Select the no. of forecasted days")
 option = st.selectbox("Select data to view",
@@ -11,19 +12,22 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures,
-                     labels={"x": "Dates", "y": "Temperature(C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                         labels={"x": "Dates", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_path = [images[condition] for condition in sky_conditions]
-        print(sky_conditions)
-        st.image(image_path, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_path, width=115)
+    except KeyError:
+        st.write("Place does not exist")
