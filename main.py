dd23a5fb6aae281435752c931a75416c734312e5
Lokesh <lokeshb031@gmail.com>
2023-11-02 03:39:18 +0530

    Made changes to Sky option by added images.

--- main.py
+++ main.py
@@ -10,10 +10,20 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures,
+                     labels={"x": "Dates", "y": "Temperature(C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t,
-                 labels={"x": "Dates", "y": "Temperature(C)"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_path = [images[condition] for condition in sky_conditions]
+        print(sky_conditions)
+        st.image(image_path, width=115)
